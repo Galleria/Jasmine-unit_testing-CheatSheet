f24bb9fcd27fc33941ff24d7d82a4899135eccab
Supachai <group1034@gmail.com>
2016-09-15 22:03:01 +0700

    Update UniqueArray.js

--- src/UniqueArray.js
+++ src/UniqueArray.js
@@ -2,9 +2,15 @@ module.exports = unique;
 
 function unique(arr){
   var duplicate_count = [];
+  var duplicate_count_minus = [];
 
   arr.forEach( (value) => {
-    duplicate_count[value] =  duplicate_count[value] === undefined ? 1 : duplicate_count[value] += 1  ;
+    //console.log( value );
+      if(value >= 0){
+        duplicate_count[value] = duplicate_count[value] === undefined ? 1 : duplicate_count[value] += 1  ;
+      }else{
+        duplicate_count_minus[-1*value] = duplicate_count_minus[-1*value] === undefined ? 1 : duplicate_count_minus[-1*value] += 1  ;
+      }
   })
 
   duplicate_count.forEach( (value,idx)=>{
@@ -14,10 +20,19 @@ function unique(arr){
       }
     }
   })
+  
+  duplicate_count_minus.forEach( (value,idx)=>{
+    if( value == 1 ){
+      while( arr.indexOf(-1*idx) > -1 ){
+        arr.splice(arr.indexOf(idx), 1);
+      }
+    }
+  })
 
   return arr;
 }
 
+unique([-10,10,0]) );
 unique([1, 2, 3, 1, 3]);
 unique([1, 2, 3, 4, 5]);
 unique([5, 5, 5, 5, 5]);
