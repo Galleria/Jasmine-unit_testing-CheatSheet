542764550bc6cf172c8e0f72ec12e45dbd2f71db
Supachai <group1034@gmail.com>
2016-09-25 09:52:39 +0700

    commited first completely task.

--- spec/MorseClockSpec.js
+++ spec/MorseClockSpec.js
@@ -2,9 +2,27 @@ var morseClock = require('../src/MorseClock')
 
 describe( "" ,()=> {
 
-  it("should be return ",()=>{
-    let time = "10:37:49";
-    let morseTime = ".- .... : .-- .--- : -.. -..-";
+  it("10:37:49 should be return ",()=>{
+    let time = '10:37:49';
+    let morseTime = '.- .... : .-- .--- : -.. -..-';
+    expect( morseClock(time) ).toEqual( morseTime );
+  })
+
+  it("21:34:56 should be return ",()=>{
+    let time = '21:34:56';
+    let morseTime = '-. ...- : .-- .-.. : -.- .--.';
+    expect( morseClock(time) ).toEqual( morseTime );
+  })
+
+  it("00:1:02 should be return ",()=>{
+    let time = '00:1:02';
+    let morseTime = '.. .... : ... ...- : ... ..-.';
+    expect( morseClock(time) ).toEqual( morseTime );
+  })
+
+  it("23:59:59 should be return ",()=>{
+    let time = '23:59:59';
+    let morseTime = '-. ..-- : -.- -..- : -.- -..-';
     expect( morseClock(time) ).toEqual( morseTime );
   })
 
