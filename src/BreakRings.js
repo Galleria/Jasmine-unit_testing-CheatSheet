340777fad4f0cee9724ebe9d13f1ae7388f3cbc9
Supachai <group1034@gmail.com>
2016-09-25 15:57:23 +0700

    sloved completely

--- src/BreakRings.js
+++ src/BreakRings.js
@@ -4,26 +4,13 @@ var breakRings = function( data ){
 
   while( rings.length > 0 ){
     let depenRings = [];
-    depenRings[0] = 0;
 
     // check ring is only one
-    if( rings.length === 1 ){
-      if( rings[0].length === 1 ){
+    if( rings.length === 1 && rings[0].length === 1){
         break;
-      }
     }
 
-    // count maximum dependent ring
-    for( let ring of rings ){
-      for( let number of ring ){
-        depenRings[ number ] = ( depenRings[ number ] === undefined ? 1 : depenRings[ number ] += 1 ) ;
-      }
-    }
-
-    // insert null dependent ring to 0
-    for( let idx=0 ; idx< depenRings.length ; idx++ ){
-        depenRings[ idx ] = ( depenRings[ idx ] === undefined ? 0 : depenRings[ idx ] ) ;
-    }
+    depenRings = defineDepenRings( rings );
 
     let maxDuplicateValue = Math.max(...depenRings);
     let number = depenRings.indexOf(maxDuplicateValue);
@@ -35,9 +22,17 @@ var breakRings = function( data ){
         maxDuplicateArr.push( pos );
     });
 
-    let maxSecondDuplicateValue = depenRings.sort( (a,b)=> { b-a })[1];
-    if( maxDuplicateValue ===3 && maxSecondDuplicateValue === 2 ){
-      number = depenRings.indexOf(maxSecondDuplicateValue);
+    let maxSecondDuplicateArr = [];
+    let maxSecondDuplicateValue = depenRings.slice().sort().reverse()[1];
+    depenRings.forEach(function(value, pos) {
+      if( value === maxSecondDuplicateValue )
+        maxSecondDuplicateArr.push( pos );
+    });
+
+    if( (maxDuplicateValue===3 && maxSecondDuplicateValue===2) || (maxDuplicateValue===4 && maxSecondDuplicateValue ===3) ){
+      if( maxDuplicateArr.length == 1 && (maxSecondDuplicateArr.length < 4 || rings.length > 10) ){
+        number = depenRings.indexOf(maxSecondDuplicateValue);
+      }
     }
 
     // cut off duplicate value ring
@@ -87,14 +82,40 @@ var breakRings = function( data ){
   return round;
 }
 
+var defineDepenRings = (rings)=> {
+  let depenRings = [];
+  depenRings[0] = 0;
+
+  // count maximum dependent ring
+  for( let ring of rings ){
+    for( let number of ring ){
+      depenRings[ number ] = ( depenRings[ number ] === undefined ? 1 : depenRings[ number ] += 1 ) ;
+    }
+  }
+
+  // insert null dependent ring to 0
+  for( let idx=0 ; idx< depenRings.length ; idx++ ){
+      depenRings[ idx ] = ( depenRings[ idx ] === undefined ? 0 : depenRings[ idx ] ) ;
+  }
+
+  return depenRings;
+}
+
+
+console.log( breakRings( [[9]] ) )   //0
+
+console.log( breakRings([[6,1],[6,5]])  )    //1
+console.log( breakRings([[6,1]])  )    //1
+
 console.log( breakRings([[1,2],[2,3],[3,4],[4,5],[4,6],[6,5]]) )   //3
 console.log( breakRings([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [4, 6]] ) )   //3
 console.log( breakRings([[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]) )    //3
 console.log( breakRings([[5, 6], [4, 5], [3, 4], [3, 2], [2, 1], [1, 6]] ) )   //3
+
 console.log( breakRings([[8, 9], [1, 9], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [8, 7]]) )  //5
-console.log( breakRings([[6,1],[6,5]])  )    //1
-console.log( breakRings([[6,1]])  )    //1
-console.log( breakRings( [[9]] ) )   //0
 console.log( breakRings( [[1,9],[1,2],[8,5],[4,6],[5,6],[8,1],[3,4],[2,6],[9,6],[8,4],[8,3],[5,7],[9,7],[2,3],[1,7]] ) )   //5
 console.log( breakRings( [[3,4],[5,6],[2,7],[1,5],[2,6],[8,4],[1,7],[4,5],[9,5],[2,3],[8,2],[2,4],[9,6],[5,7],[3,6],[1,3]] ) )   //5
+
+console.log( breakRings([[2,5],[3,7],[5,6],[6,7],[9,6],[8,9],[9,7],[1,4],[1,9],[9,5],[2,4],[2,6],[2,3],[9,2],[3,6],[4,5],[1,2]]) )  //5
+
 console.log( breakRings([[1,2],[2,3],[3,4],[4,5],[5,2],[1,6],[6,7],[7,8],[8,9],[9,6],[1,10],[10,11],[11,12],[12,13],[13,10],[1,14],[14,15],[15,16],[16,17],[17,14]]) ); // 8
